f0bff76b5d891ebd1130a44380dce3c4f633ebdf
4123-WS <devs@frc4123.org>
2016-10-18 16:18:59 -0700

    Initial commit

---
+++ src/org/frc4123/robot/demo/scrappy/Robot.java
@@ -0,0 +1,93 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.frc4123.robot.demo.scrappy;
+
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+    
+    
+    //Drive
+    Jaguar scFL = new Jaguar(1);
+    Jaguar scBL = new Jaguar(2);
+    Jaguar scFR = new Jaguar(3);
+    Jaguar scBR = new Jaguar(4);
+    Joystick joyDrive = new Joystick(1);
+    SafeRobotDrive drive = new SafeRobotDrive(scFL, scBL, scFR, scBR);
+    
+    double mDriveForward = 0;
+    double mDriveRotate = 0;
+    
+    //Safety constants
+    final static double kSpeedmodDefault = .65;
+    final static double kturnmoddefault=.80;
+
+    
+    
+    
+    
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+        //TREVOR: Watchdog temproarily stops the robot if it doesn't get a response every 100ms. The line below disables that, so it'll make the robot less jerky, but it also gives it the potential to run off on its own, even after you disable it. Use at your own discretion.
+        //drive.setSafetyEnabled(false);
+
+        //These set which motors should be reversed. Set two on the same side to true.
+        drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, false);
+        drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, false);
+        drive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
+        drive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
+        
+        drive.setSpeedmodifier(kSpeedmodDefault);
+        drive.setTurnmodifier(kturnmoddefault);
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+
+    }
+
+    /**
+     * 
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {       
+
+     
+
+        
+        drive.safeArcadeDrive(joyDrive.getRawAxis(5), joyDrive.getRawAxis(2), true);
+    }
+    
+    public void testInit(){
+        drive.setSpeedmodifier(1);
+    }
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+        drive.arcadeDrive(.35, 0);
+    }
+    
+}
---
+++ src/org/frc4123/robot/demo/scrappy/SafeRobotDrive.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.frc4123.robot.demo.scrappy;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
+
+/**
+ *
+ * @author Adrian Campos - frc4123
+ */
+public class SafeRobotDrive extends RobotDrive {
+   
+        private double speedmodifier = 1;
+        private double turnmodifier = 1;
+        
+	public SafeRobotDrive(SpeedController FL, SpeedController BL, SpeedController FR, SpeedController BR) {
+		super(FL, BL, FR, BR);
+		
+	}
+	
+	public void safeArcadeDrive(double moveValue, double rotateValue, boolean squaredinputs){
+		super.arcadeDrive(this.speedmodifier*moveValue, this.turnmodifier*rotateValue, squaredinputs);
+	}
+	public void safeArcadeDrive(double moveValue, double rotateValue){
+		super.arcadeDrive(this.speedmodifier*moveValue, this.turnmodifier*rotateValue);
+	}
+        
+	public void setSpeedmodifier(double speedmodifier){
+            this.speedmodifier = speedmodifier;
+        }
+        public double getSpeedmodifier(){
+            return this.speedmodifier;
+        }
+        
+        public void setTurnmodifier(double turnmodifier){
+            this.turnmodifier = turnmodifier;
+        }
+	public double getTurnmodifier(){
+            return this.turnmodifier;
+        }
+}
+
+
